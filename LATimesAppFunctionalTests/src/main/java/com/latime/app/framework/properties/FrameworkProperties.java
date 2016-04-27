027a1a11b6a27f690e05496d95b4895503f52fda
chaitanyak <chaitanyakulkarni@outlook.com>
2016-04-27 18:03:37 +0530

    changed IP address

--- LATimesAppFunctionalTests/src/main/java/com/latime/app/framework/properties/FrameworkProperties.java
+++ LATimesAppFunctionalTests/src/main/java/com/latime/app/framework/properties/FrameworkProperties.java
@@ -4,7 +4,7 @@ public class FrameworkProperties {
 	//Samarth's machine IP
 //		public static final String APPIUM_ANDROID_SERVER_IP = "10.20.121.69";
 	
-	//Chaitanya's machine IP
+	//Chaitanya's machine IP address
 		public static final String APPIUM_ANDROID_SERVER_IP = "10.20.121.77";
 		
 }
